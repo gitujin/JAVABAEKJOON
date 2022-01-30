384cb7540dafdc1fb11b9f82ac0b1c62ebf2fe4d
gitujin <syj991114@gmail.com>
2022-01-30 23:33:56 +0900

    2 commit

---
+++ STEP2/src/test1/Main.java
@@ -0,0 +1,20 @@
+package test1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int value = a*b*c;
+		
+		
+		
+	}
+
+}
