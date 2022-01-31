867ad283e5aaf9f8dc08f04ca87209e6727b330c
gitujin <syj991114@gmail.com>
2022-01-31 15:06:54 +0900

    3 commit

--- STEP2/src/test1/Main.java
+++ STEP2/src/test1/Main.java
@@ -5,16 +5,27 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		
-		int value = a*b*c;
-		
-		
-		
-	}
 
+		int count = 0;
+		int[] val = new int[10];
+		
+		for(int i=0; i<val.length;i++) {
+			val[i] = (sc.nextInt()%42);
+		}
+		
+		for(int i = 0; i<val.length; i++) {
+			int cnt = 0;
+			for(int j = i+1; j<val.length; j++) {
+				if(val[i] == val[j]) {
+					cnt++;
+				}
+
+		}
+			if(cnt == 0) { 
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
 }
