96410e4ce61360d79913377865d61c217d4beffd
gitujin <syj991114@gmail.com>
2022-05-27 15:41:54 +0900

    10809번

--- STEP1/src/test1/Main.java
+++ STEP1/src/test1/Main.java
@@ -5,12 +5,26 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		while (sc.hasNextInt()) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			System.out.println(a+b);
+		Scanner sc= new Scanner(System.in);
+		
+		int[] array = new int[26];
+		
+		for(int i=0; i<array.length; i++) {
+			array[i] = -1;
 		}
-		sc.close();
+		
+		String s = sc.nextLine();
+		
+		for(int i=0; i<s.length();i++) {
+			char ch = s.charAt(i);
+			
+			if(array[ch-'a'] == -1) {
+				array[ch - 'a'] = i;
+			}
+		}
+		
+		for(int val : array) {
+			System.out.print(val+ " ");
+		}
 	}
 }
