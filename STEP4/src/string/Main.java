0998177374a525bd4c723d65d45cad59d4f2495f
gitujin <syj991114@gmail.com>
2022-05-29 20:46:57 +0900

    2675번

--- STEP4/src/string/Main.java
+++ STEP4/src/string/Main.java
@@ -7,8 +7,21 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		String s = sc.next();
+		int A = sc.nextInt();
+		
+		for(int i = 0; i<A; i++) {
+			
+			int R = sc.nextInt();
+			String S = sc.next();
+			
+			for(int j = 0; j<S.length(); j++) {
+				for(int k = 0; k<R; k++) {
+					System.out.print(S.charAt(j));
+				}
+			}
+			
+			System.out.println();
+		}
 	}
-
 }
                                                                                                                                                                                                                                                                                                                                                                                                                              
