3da177870f51cf520ab22ac5cc2d130653ce122e
gitujin <syj991114@gmail.com>
2022-06-02 21:42:49 +0900

    5622번

--- STEP4/src/string/Main.java
+++ STEP4/src/string/Main.java
@@ -8,14 +8,47 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int A = sc.nextInt();
-		int B = sc.nextInt();
+		String s = sc.nextLine(); // 입력받은 문자열
 		
-		sc.close();
+		int count = 0; // 걸리는 시간
 		
-		A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
-		B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
-		
-		System.out.println(A>B?A:B);
+		for(int i=0; i<s.length();i++) { //s의 길이 만큼 반복
+			
+			switch(s.charAt(i)) { //s 문자열 0부터 끝까지
+				
+			case'A' : case'B' : case'C':
+				count += 3;
+				break;
+				
+			case'D' : case'E' : case'F':
+				count += 4;
+				break;
+				
+			case'G' : case'H' : case'I':
+				count += 5;
+				break;
+				
+			case 'J' : case 'K': case 'L' : 
+				count += 6;
+				break;
+                
+			case 'M' : case 'N': case 'O' :
+				count += 7;
+				break;
+                
+			case 'P' : case 'Q': case 'R' : case 'S' : 
+				count += 8; 
+				break;
+                
+			case 'T' : case 'U': case 'V' : 
+				count += 9;
+				break;
+                
+			case 'W' : case 'X': case 'Y' : case 'Z' : 
+				count += 10;
+				break;
+			}
+		}
+		System.out.print(count);
 	}
 }
